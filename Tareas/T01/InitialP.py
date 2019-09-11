0cf97e8911f5f99a0ef849df95ac6eed5f219ae0
David Esteban Arias Hernandez <david.arias@uc.cl>
2019-09-11 02:44:04 -0400

    I'm making some progress here 0 w 0

--- Tareas/T01/InitialP.py
+++ Tareas/T01/InitialP.py
@@ -1,41 +1,43 @@
 from abc import ABC, abstractmethod
 from random import random, randint as random, randint
+from math import ceil as ceil
 from os import path
 import parametros
+import funciones
+import sys.exit
 number_team = {0: "Tareos", 1: "Hibridos", 2: "Docencios"}
-number_car = {0: "Automovil", 1: "Troncomovil", 2: "Motocicleta", 3: "Bicicleta"}
-car_parametros = {"Automovil": parametros.AUTOMOVIL, "Bicicleta": parametros.BICICLETA,
-                  "Troncomovil": parametros.TRONCOMOVIL, "Motocicleta": parametros.MOTOCICLETA}
+number_car = {0: "automóvil", 1: "troncomóvil", 2: "motocicleta", 3: "bicicleta"}
+car_parametros = {"automóvil": parametros.AUTOMOVIL, "bicicleta": parametros.BICICLETA,
+                  "troncomóvil": parametros.TRONCOMOVIL, "motocicleta": parametros.MOTOCICLETA}
 racer_parametros = {"Tareos": parametros.EQUIPOS["TAREOS"],
                     "Docencios": parametros.EQUIPOS["DOCENCIOS"],
                     "Hibridos": parametros.EQUIPOS["HIBRIDOS"]}
-path_racers = parametros.PATHS["CONTRINCANTES"]
-path_cars = parametros.PATHS["VEHICULOS"]
 
 
 class Menu(ABC):
     def __init__(self, opciones, salir, funciones):
         self.options = opciones
+        self.option = None
         self.dict = dict(zip(range(1, len(opciones) + 1), opciones))
 
     def print(self):
-        for index, option in zip(range(1, len(self.options + 1)), self.options):
-            print(f"[{index}] {option}")
-        print(f"[0] {self.salir}")
+        print_options(self.options)
+        print(f"\n[0] {self.salir}")
 
     def selection(self):
-        self.print()
-        comand = None
-        while not comand.isnumeric() and len(comand) > 1:
+        command = " "
+        while not command.isnumeric() and len(command) == 1:
+            self.print()
             command = input("\n")
-            if not comand.isnumeric():
+            if not command.isnumeric():
                 error(1)
-            elif len(comand) > 1:
+            elif len(command) > 1:
                 error(2)
-        if int(comand) in self.dict.keys():
-            self.dict(int(comand))()
+        if int(command) in self.dict.keys():
+            self.option = self.dict(int(command))
+            return True
         else:
-            exit()
+            return False
 
 
 class Menu_sesion(Menu):
@@ -50,7 +52,32 @@ class Menu_principal(Menu):
 
 class Menu_buy():
     def __init__(self, options):
-        super().__init__(options)
+        super().__init__(*opciones, ("automóvil", "troncomóvil",
+                         "motocicleta", "bicicleta"))
+
+    def print(self):
+        for index, option in zip(range(1, len(self.options + 1)), self.options):
+            print(f"[{index}] {option[0]}    -${option[1]}-")
+        print(f"\n[0] {self.salir}")
+
+    def selection(self):
+        command, cost = " ", 0
+        while not (command.isnumeric() and juego.money >= self.options[int(comand)][1]):
+            print("Vehículos disponibles para comprar:")
+            self.print()
+            command = input("\n")
+            if not command.isnumeric():
+                error(1)
+            elif len(command) > 1:
+                error(2)
+            elif int(command) < self.options[int(command)][1]:
+                error(4)
+                comand = " "
+        if command in self.dict.keys():
+            self.option = (self.dict[int(command)], self.options[int(command)][1])
+            return True
+        else:
+            return False
 
 
 class Menu_pits(Menu):
@@ -58,7 +85,32 @@ class Menu_pits(Menu):
         super().__init__(options)
 
 
-class Menu_carrera(Menu):
+class Menu_race_configuration(Menu):
+    def __init__(self):
+        pass
+
+    def selection(self):
+        while True:
+            print("Elige la pista en la que deseas competir:")
+            print_options(juego.tracks)
+            pista = check_numeric()
+            if pista and 0 < pista < len(juego.tracks) + 1:
+                juego.actual_track = juego.tracks[pista - 1]
+                break
+            else:
+                error(5)
+        while True:
+            print("Elige el vehículo con el que deseas competir:")
+            print_options(juego.user_cars)
+            vehiculo = check_numeric()
+            if pista and 0 < pista < len(juego.tracks) + 1:
+                juego.actual_car = juego.user_cars[pista - 1]
+                break
+            else:
+                error(5)
+
+
+class Menu_race(Menu):
     def __init__(self, *opciones):
         super().__init__(*opciones, (juego.continue_race))
 
@@ -66,13 +118,13 @@ class Menu_carrera(Menu):
 class Racers():
     def __init__(self, name, money, personalidad, cuerpo, balance,
                  exp, equipo):
-        self.experience = exp
+        self.experience = int(exp)
         self.name = name
-        self.money = money
+        self.money = int(money)
         self.team = equipo
         self.personality = personalidad
-        self.body = cuerpo
-        self.balance = balance
+        self.body = int(cuerpo)
+        self.balance = int(balance)
 
     def __repr__(self):
         return self.name
@@ -83,11 +135,75 @@ class Cars():
         self.name = name
         self.owner = dueno
         self.type = tipo
-        self.chassis = chasis
-        self.body = carroceria
-        self.weight = peso
-        self.wheels = ruedas
-        self.motor = aceleracion
+        self.chassis = int(chasis)
+        self.chassis_buff = 0
+        self.body = int(carroceria)
+        self.body_buff = 0
+        self.weight = int(peso)
+        self.weight_buff = 0
+        self.wheels = int(ruedas)
+        self.wheels_buff = 0
+        self.engine = int(aceleracion)
+        self.engine_buff = 0
+        self.damage = 0
+
+    @property
+    def chasis(self):
+        return self.chassis + self.chassis_buff * parametros.MEJORAS["CHASIS"]
+
+    @chasis.setter
+    def chasis(self, x):
+        self.chassis_buff += x
+
+    @property
+    def carroceria(self):
+        return self.body + self.body_buff * parametros.MEJORAS["CARROSERIA"]
+
+    @carroceria.setter
+    def carroceria(self, x):
+        self.body_buff += x
+
+    @property
+    def peso(self):
+        return self.weight + self.weight_buff * parametros.MEJORAS["PESO"]
+
+    @peso.setter
+    def peso(self, x):
+        self.weight_buff += x
+
+    @property
+    def ruedas(self):
+        return self.wheels + self.wheels_buff * parametros.MEJORAS["RUEDAS"]
+
+    @ruedas.setter
+    def ruedas(self, x):
+        self.wheels_buff += x
+
+    @property
+    def motor(self):
+        if self.type in ("automóvil", "troncomóvil"):
+            return self.engine + self.engine_buff * parametros.MEJORAS["MOTOR"]
+        else:
+            return self.engine + self.engine_buff * parametros.MEJORAS["ZAPATILLAS"]
+
+    @motor.setter
+    def motor(self, x):
+        self.motor_buff += x
+
+    def __repr__(self):
+        return self.name
+
+
+class Tracks():
+    def __init__(self, name, tipo, hielo, rocas, dificultad, rivales, vueltas, largo):
+        self.name = name
+        self.type = tipo
+        self.ice = int(hielo)
+        self.rocks = int(rocas)
+        self.level = int(dificultad)
+        self.enemies = rivales
+        self.laps = int(vueltas)
+        self.length = int(largo)
 
     def __repr__(self):
         return self.name
@@ -97,49 +213,57 @@ class Game():
     def __init__(self):
         self.user = None
         self.actual_car = None
+        self.actual_track = None
         self.user_cars = []
-        self.dinero = 0
-        self.racers = load_racers()
-        self.cars = load_cars()
+        self.money = 0
+        self.users = funciones.load_pilotos()
+        self.enemies = funciones.load_enemies()
+        self.cars = funciones.load_cars()
+        self.tracks = funciones.load_tracks()
+        self.lap = 0
 
     def new_game(self):
-        name, team, car, carname = None, None, None, None
-        while not "".join(filter(lambda character: character != " ", name)).isalpha():
-            name = input("Selecciona un nombre de usuario:\n")
-            if not "".join(filter(lambda character: character != " ", name)).isalpha():
-                error(0)
-        while not team.isnumeric():
+        del self.pilotos
+        name, team, car, carname, isalpha = "", "", False, "", False
+        while True:
+            name = check_alpha("Selecciona un nombre de usuario:\n")
+            if name:
+                if name in self.pilotos:
+                    error(6)
+                else:
+                    break
+        while True:
             print("Selecciona uno de los siguientes equipos",
                   "disponibles:\n")
             print_options(("Tareos", "Hibridos", "Docencios"))
-            team = input()
-            if team.isnumeric():
-                self.user = new_racer(name, number_team[int(team)])
-                with open(path_racers, "a", encoding="utf-8") as file:
-                    file.write(f"{name},{self.user.money},{self.user.personality}," +
-                               f"{self.user.body},{self.user.balance},{self.user.exp}" +
-                               f",{self.user.team}")
-            else:
-                error(1)
-        while not "".join(filter(lambda character: character != " ", carname)).isalpha():
+            team = check_numeric(input())
+            if team:
+                if int(team) in (1, 2, 3):
+                    self.user = funciones.new_racer(name, number_team[int(team)])
+                    with open(parametros.PATHS["PILOTOS"], "a", encoding="utf-8") as file:
+                        file.write(f"{name},{self.user.money},{self.user.personality}," +
+                                   f"{self.user.body},{self.user.balance},{self.user.exp}" +
+                                   f",{self.user.team}")
+                    break
+                else:
+                    error(5)
+        while True:
             print("Selecciona el tipo de tu primer vehículos:\n")
             print_options(("Automovil", "Troncomovil", "Motocicleta", "Bicicleta"))
-            if car is None:
-                while not "".join(filter(lambda character: character != " ", car)).isalpha():
-                    car = input()()
-                    if not "".join(filter(lambda character: character != " ", car)).isalpha():
-                        error(1)
-            carname = input("Selecciona un nombre para tu vehiculo:\n")
-            if "".join(filter(lambda character: character != " ", carname)).isalpha() \
-               and carname not in self.user_cars:
-                self.user_cars.append(self.actual_car)
+            if car is False:
+                while car:
+                    car = check_numeric()
+            carname = check_alpha("Selecciona un nombre para tu vehiculo:\n")
+            if carname and carname not in self.user_cars:
+                self.user_cars.append(funciones.new_car(carname, number_car[car]))
                 with open(path_cars, "a", encoding="utf-8") as file:
                     file.write(f"{carname},{name},{self.actual_car.type}," +
                                f"{self.actual_car.chassis},{self.actual_car.body}," +
                                f"{self.actual_car.wheels}, {self.actual_car.motor}" +
                                f",{self.actual_car.shoes},{self.actual_car.weight}")
-            else:
-                error(0)
+                break
+            elif carname in self.user_cars:
+                error(6)
 
     def save_game(self):
         pass
@@ -148,59 +272,49 @@ class Game():
         pass
 
     def start_race(self):
-        pass
+        menu_rc.selection()
+        while True:
+            menu_r.selection()
 
     def buy_car(self):
-        pass
+        while True:
+            if menu_b.selection():
+                while True:
+                    name = check_alpha()
+                    if name in self.user_cars:
+                        error(6)
+                    elif name:
+                        break
+                funciones.new_car(name, menu_b.option[0])
+                self.money -= menu_b.option[1]
+            else:
+                break
 
     def continue_race(self):
         pass
 
 
-def new_car(nombre, tipo):
-    max_min = car_parametros[tipo]
-    chasis = randint(max_min["CHASIS"]["MIN"], max_min["CHASIS"]["MAX"])
-    carroceria = randint(max_min["CARROCERIA"]["MIN"], max_min["CARROCERIA"]["MAX"])
-    peso = randint(max_min["PESO"]["MIN"], max_min["PESO"]["MAX"])
-    ruedas = randint(max_min["RUEDAS"]["MIN"], max_min["RUEDAS"]["MAX"])
-    zapatillas = randint(max_min["ZAPATILLAS"]["MIN"], max_min["ZAPATILLAS"]["MAX"])
-    motor = randint(max_min["MOTOR"]["MIN"], max_min["MOTOR"]["MAX"])
-    return Cars(nombre, tipo, chasis, carroceria, peso, ruedas, zapatillas, motor)
-
-
-def new_racer(name, equipo):
-    max_min = racer_parametros[equipo]
-    contextura = randint(max_min["CONTEXTURA"]["MIN"], max_min["CONTEXTURA"]["MAX"])
-    equilibrio = randint(max_min["EQUILIBRIO"]["MIN"], max_min["EQUILIBRIO"]["MAX"])
-    personalidad = max_min["PERSONALIDAD"]
-    return Racers(name, 0, contextura, equilibrio, personalidad, 0, equipo)
-
-
-def load_racers():
-    with open(path_racers, "r", encoding="utf-8") as file:
-        racers = file.readlines()
-    list_racers = []
-    for line in racers[1:]:
-        racer = line[:-1].split(",")
-        list_racers.append(Racers(racer[0], racer[1], racer[2], racer[3],
-                                  racer[4], racer[5], racer[6]))
-    return list_racers
-
-
-def load_cars():
-    with open(path_cars, "r", encoding="utf-8") as file:
-        racers = file.readlines()
-    list_cars = []
-    for line in racers[1:]:
-        racer = line[:-1].split(",")
-        list_cars.append(Racers(racer[0], racer[1], racer[2], racer[3],
-                         racer[4], racer[5], racer[6]))
-    return list_cars
-
-
 def print_options(options):
-    for index, option in zip(range(len(options)), options):
-        print(f"[{index}] {option}\n")
+    for index, option in zip(range(1, len(options + 1)), options):
+            print(f"[{index}] {option[0]}\n")
+
+
+def check_alpha(mensaje):
+    command = input(mensaje).strip(" ")
+    if not "".join(filter(lambda character: character != " ", command)).isalpha():
+        error(0)
+        return False
+    else:
+        return command
+
+
+def check_numeric(mensaje):
+    command = input(mensaje).strip(" ")
+    if not "".join(filter(lambda character: character != " ", command)).isnumeric():
+        error(1)
+        return False
+    else:
+        return command
 
 
 def error(option):
@@ -208,15 +322,28 @@ def error(option):
     option2 = "El valor ingresado no contiene solo numeros"
     option3 = "El nombre elegido ya lo posee otro de tus vehiculos"
     option4 = "El comando posee una longitud erronea"
-    options = {0: option1, 1: option2, 2: option3, 3: option4}
+    option5 = "No posees el dinero necesario para realizar esa compra"
+    option6 = "El valor ingresado es mayor o menor a los valores posibles"
+    option7 = "Este nombre ya lo posee otro piloto"
+    options = {0: option1, 1: option2, 2: option3, 3: option4, 4: option5,
+               5: option6, 6: option7}
     print(f"\n --Error-- {options[option]}\n")
 
-juego = Game()
-menu_s = Menu_sesion(("Nueva partida", "Cargar partida") ,"Salir")
-menu_pp = Menu_principal(("Nueva Carrera", "Guardar partida", "Comprar vehículo"), "Salir")
-menu_c = Menu_carrera("Ir a los pits", "Continuar la carrera")
-#menu_pt = Menu_pits(())
+
 if __name__ == "__main__":
-    tocomocho = new_car("test", "Automovil")
-    print(tocomocho)
-    print(tocomocho.motor)
+    juego = Game()
+    menu_s = Menu_sesion(("Nueva partida", "Cargar partida"), "Salir")
+    menu_pp = Menu_principal(("Nueva Carrera", "Guardar partida", "Comprar vehículo"), "Salir")
+    menu_b = Menu_buy(("Automóvil", 500), ("Troncomóvil", 900),
+                      ("Motocicleta", 370), ("Bicicleta", 1050), "Volver al menú principal")
+    menu_r = Menu_race("Ir a los pits", "Continuar la carrera")
+    menu_rs = Menu_race_configuration()
+    menu_dict = {0: menu_s, 1: menu_pp}
+    estado = 0
+    while True:
+        if menu_dict[estado].selection():
+            menu_dict[estado].option()
+            if estado == 0:
+                estado += 1
+        else:
+            sys.exit(0)
---
+++ Tareas/T01/funciones.py
@@ -0,0 +1,134 @@
+def velocidad_real(car, racer, track):
+    max(parametros.VELOCIDAD_MINIMA, velocidad_intencional(car, racer, track) +
+        hipotermia(racer, track) + dificultad_control(car, racer))
+
+
+def dificultad_control(car, racer):
+    if car.type in ("Troncomovil", "Automovil"):
+        return 0
+    else:
+        if racer.personality == "precavido":
+            balance_multiplier = parametros.EQUILIBRIO_PRECAVIDO
+        else:
+            balance_multiplier = 1
+        return min(0, racer.balance * balance_multiplier -
+                   (parametros.PESO_MEDIO/car.weigth)//1)
+
+
+def velocidad_intencional(car, racer, track):
+    if racer.personality == "precavido":
+        speed_multiplier = parametros.EFECTO_PRECAVIDO
+    else:
+        speed_multiplier = parametros.EFECTO_OSADO
+    return velocidad_multiplier * velocidad_recomendada(racer, car, track)
+
+
+def velocidad_recomendada(car, racer, track):
+    return car.motor + (car.ruedas - track.ice) * parametros.POND_EFECT_HIELO +\
+           (car.chasis - track.rocks) * parametros.POND_EFECT_ROCAS +\
+           (racer.exp - track.level) * parametros.POND_EFECT_DIFICULTAD
+
+
+def hipotermia(racer, track):
+    return min(0, juego.lap * (racer.body - track.ice))
+
+
+def car_damage(car, track):
+    return max(0, track.rocks - car.carroseria)
+
+
+def pits_time(car):
+    return parametros.TIEMPO_MINIMO_PITS + (car.damage) * parametros.VELOCIDAD_PITS
+
+
+def money_per_lap(track):
+    return juego.lap * track.level
+
+
+def lap_time(car, racer, track):
+    return ceil(track.lenght/velocidad_real(car, racer, track))
+
+
+def accident(car, racer, track):
+    speed_recommended = velocidad_recomendada(car, racer, track)
+    speed_real = velocidad_real(car, racer, track)
+    return min(1, max(0, (speed_real - speed_recommended)/speed_recommended) +
+               (car.damage/car.chasis)//1)
+
+
+def winer_price(track):
+    return juego.laps * (track.level + track.ice - track.rocks)
+
+
+def winer_exp(racer, track, last_racer):
+    if racer.personality == "precavido":
+        exp_multiplier = parametros.BONIFICACION_PRECAVIDO
+    else:
+        exp_multiplier = parametros.BONIFICACION_OSADO
+    return (track.level + advantage_last_place(racer, last_racer)) * exp_multiplier
+
+
+def advantage_last_place(racer, last_place):
+    return racer.time - last_place.time
+
+
+def new_car(nombre, tipo):
+    max_min = car_parametros[tipo]
+    chasis = randint(max_min["CHASIS"]["MIN"], max_min["CHASIS"]["MAX"])
+    carroceria = randint(max_min["CARROCERIA"]["MIN"], max_min["CARROCERIA"]["MAX"])
+    peso = randint(max_min["PESO"]["MIN"], max_min["PESO"]["MAX"])
+    ruedas = randint(max_min["RUEDAS"]["MIN"], max_min["RUEDAS"]["MAX"])
+    zapatillas = randint(max_min["ZAPATILLAS"]["MIN"], max_min["ZAPATILLAS"]["MAX"])
+    motor = randint(max_min["MOTOR"]["MIN"], max_min["MOTOR"]["MAX"])
+    return Cars(nombre, tipo, chasis, carroceria, peso, ruedas, zapatillas, motor)
+
+
+def new_racer(name, equipo):
+    max_min = racer_parametros[equipo]
+    contextura = randint(max_min["CONTEXTURA"]["MIN"], max_min["CONTEXTURA"]["MAX"])
+    equilibrio = randint(max_min["EQUILIBRIO"]["MIN"], max_min["EQUILIBRIO"]["MAX"])
+    personalidad = max_min["PERSONALIDAD"]
+    return Racers(name, 0, contextura, equilibrio, personalidad, 0, equipo)
+
+
+def load_enemies():
+    with open(parametros.PATHS["CONTRINCANTES"], "r", encoding="utf-8") as file:
+        racers = file.readlines()
+    list_enemies = []
+    for line in racers[1:]:
+        racer = line[:-1].split(",")
+        list_racers.append(Racers(racer[0], racer[1], racer[2], racer[3],
+                                  racer[4], racer[5], racer[6]))
+    return list_enemies
+
+
+def load_cars():
+    with open(parametros.PATHS["VEHICULOS"], "r", encoding="utf-8") as file:
+        racers = file.readlines()
+    list_cars = []
+    for line in racers[1:]:
+        racer = line[:-1].split(",")
+        list_cars.append(Racers(racer[0], racer[1], racer[2], racer[3],
+                         racer[4], racer[5], racer[6]))
+    return list_cars
+
+
+def load_tracks():
+    with open(parametros.PATHS["PISTAS"], "r", encoding="utf-8") as file:
+        tracks = file.readlines()
+    list_tracks = []
+    for line in tracks[1:]:
+        track = line[:-1].split(",")
+        list_tracks.append(track[0], track[1], track[2], track[3], track[4],
+                           track[5].split(";"), track[6], track[7])
+
+
+def load_racers():
+    with open(parametros.PATHS["PILOTOS"], "r", encoding="utf-8") as file:
+        racers = file.readlines()
+    list_racers = []
+    for line in racers[1:]:
+        racer = line[:-1].split(",")
+        list_tracks.append(track[0], track[1], track[2], track[3], track[4],
+                           track[5], track[6])
+    return list_racers
